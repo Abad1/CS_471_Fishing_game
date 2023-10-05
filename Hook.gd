00f112e4985cbc3ff1faa84d984c675c2b819811
Cody <caabad3@alaska.edu>
2023-10-04 16:49:55 -0800

    added moveable hook

---
+++ Hook.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("Move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("Move_down"):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)

208261d8c2c3365eb8fd8d412bc0a7f519dc4349
benstream <benjaminstream@yahoo.com>
2023-10-19 17:47:33 -0800

    Seperated spawning Entity into a function, calling entitys to spawn from that

--- Entities/Barrel/Barrel.gd
+++ Entities/Barrel/Barrel.gd
@@ -10,9 +10,13 @@ var velocity = Vector2.ZERO
 func _ready():
 	screen_size = get_viewport_rect().size
 	velocity = Vector2(1,0)
-	if position.x > screen_size/2:
+	if (position.x > 0):
+		get_node("Sprite2D").set_flip_h(true)
 		velocity.x = -1
 
+#Remove Entity after leaves screen
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
